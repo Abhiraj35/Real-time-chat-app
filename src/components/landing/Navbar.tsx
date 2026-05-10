69d8acead50005b6f5a456656ec601a57d4f412b
Abhiraj35 <royabhi2406@gmail.com>
2026-05-10 23:09:33 +0530

    fix: Update Navbar button link from "Browse Registry" to "Lobby" for improved navigation

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -33,8 +33,8 @@ const Navbar = () => (
     </div>
 
     <div className="flex items-center">
-      <Button href="#" className="hidden sm:inline-flex">
-        Browse Registry
+      <Button href="/lobby" className="hidden sm:inline-flex">
+        Lobby
       </Button>
     </div>
   </nav>
